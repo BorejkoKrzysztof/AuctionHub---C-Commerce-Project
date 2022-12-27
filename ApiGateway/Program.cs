05d583567fbe2a2397b7dc9757af36c138fff7be
KrzysztofBorejko <krzysiek.13.krzysiek@gmail.com>
2022-12-27 23:33:46 +0100

    Add configuration for logging

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -3,6 +3,10 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
+
+builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true);
 builder.Services.AddOcelot();
 
 // Add services to the container.
@@ -10,9 +14,6 @@ builder.Services.AddOcelot();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
-app.UseHttpsRedirection();
-
 app.Run();
 
 await app.UseOcelot();
