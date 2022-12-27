40eb1533e6348fdd9a828bc9cb0fbb215018eccd
KrzysztofBorejko <krzysiek.13.krzysiek@gmail.com>
2022-12-27 21:17:19 +0100

    Configure logger for webhostbuilder

--- src/ApiGateways/ApiGateway/Program.cs
+++ src/ApiGateways/ApiGateway/Program.cs
@@ -1,6 +1,9 @@
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args).WebHost.ConfigureLogging((hostingContext, loggingBuilder) =>
+{
+    loggingBuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+    loggingBuilder.AddConsole();
+    loggingBuilder.AddDebug();
+});
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
 
 app.Run();
