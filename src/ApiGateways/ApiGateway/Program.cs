d54e2d87e8232cff8627574df7fbc678888c054a
KrzysztofBorejko <krzysiek.13.krzysiek@gmail.com>
2022-12-28 13:23:59 +0100

    Add Caching in Ocelot Api Gateway

--- src/ApiGateways/ApiGateway/Program.cs
+++ src/ApiGateways/ApiGateway/Program.cs
@@ -1,3 +1,4 @@
+using Ocelot.Cache.CacheManager;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
@@ -9,7 +10,8 @@ builder.Logging.AddDebug();
 builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", 
                                     optional: true, 
                                     reloadOnChange: true);
-builder.Services.AddOcelot();
+builder.Services.AddOcelot()
+                .AddCacheManager(settings => settings.WithDictionaryHandle());
 
 // Add services to the container.
 
